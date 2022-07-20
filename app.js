4774ad6b2c9d6368c8c19395fa732a8e6e831bd9
Fernando Tello Porras <fernando.tello@unmsm.edu.pe>
2022-07-20 01:52:43 -0500

    Formulario de login y registro

--- app.js
+++ app.js
@@ -26,13 +26,103 @@ app.use(session({
 
 const connection = require('./database/db');
 
-app.get('/', (req, res) => {
-    res.render('index');
+app.get('/login', (req, res) => {
+    res.render('login');
 })
 
 app.get('/register', (req, res) => {
     res.render('register');
 })
+
+app.post('/register', async (req, res)=>{
+	const user = req.body.user;
+	const name = req.body.name;
+    const rol = req.body.rol;
+	const pass = req.body.pass;
+	let passwordHash = await bcrypt.hash(pass, 8);
+    connection.query('INSERT INTO users SET ?',{user:user, name:name, rol:rol, pass:passwordHash}, async (error, results)=>{
+        if(error){
+            console.log(error);
+        }else{            
+			res.render('register', {
+				alert: true,
+				alertTitle: "Registration",
+				alertMessage: "¡Successful Registration!",
+				alertIcon:'success',
+				showConfirmButton: false,
+				timer: 1500,
+				ruta: ''
+			});
+            //res.redirect('/');         
+        }
+	});
+})
+
+app.post('/auth', async (req, res)=> {
+	const user = req.body.user;
+	const pass = req.body.pass;    
+    let passwordHash = await bcrypt.hash(pass, 8);
+	if (user && pass) {
+		connection.query('SELECT * FROM users WHERE user = ?', [user], async (error, results, fields)=> {
+			if( results.length == 0 || !(await bcrypt.compare(pass, results[0].pass)) ) {    
+				res.render('login', {
+                        alert: true,
+                        alertTitle: "Error",
+                        alertMessage: "USUARIO y/o PASSWORD incorrectas",
+                        alertIcon:'error',
+                        showConfirmButton: true,
+                        timer: false,
+                        ruta: 'login'    
+                    });
+                //res.send('Incorrect Username and/or Password!');				
+			} else {         
+				//creamos una var de session y le asignamos true si INICIO SESSION       
+				req.session.loggedin = true;                
+				req.session.name = results[0].name;
+				res.render('login', {
+					alert: true,
+					alertTitle: "Conexión exitosa",
+					alertMessage: "¡LOGIN CORRECTO!",
+					alertIcon:'success',
+					showConfirmButton: false,
+					timer: 1500,
+					ruta: ''
+				});        			
+			}			
+			res.end();
+		});
+	} else {	
+		res.send('Please enter user and Password!');
+		res.end();
+	}
+});
+
+app.get('/', (req, res)=> {
+	if (req.session.loggedin) {
+		res.render('index',{
+			login: true,
+			name: req.session.name			
+		});		
+	} else {
+		res.render('index',{
+			login:false,
+			name:'Debe iniciar sesión',			
+		});				
+	}
+	res.end();
+});
+
+app.use(function(req, res, next) {
+    if (!req.user)
+        res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
+    next();
+});
+
+app.get('/logout', function (req, res) {
+	req.session.destroy(() => {
+	  res.redirect('/')
+	})
+});
 
 app.listen(3000, (req, res) => {
     console.log('Server running in http://localhost:3000');
